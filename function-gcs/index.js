734940735b4b6005b401e1b7c21ebd88f741b3d7
Debi Ejeta <86627766+Debi-Ejeta@users.noreply.github.com>
2024-05-08 13:34:44 -0400

    Update index.js

--- function-gcs/index.js
+++ function-gcs/index.js
@@ -1,4 +1,4 @@
-const {CloudEvents} = require('@google-cloud/functions-framework');
+const functions = require('@google-cloud/functions-framework');
 
 // Register a CloudEvent function with the Functions Framework
 exports.helloGCS = (cloudEvent) => {
