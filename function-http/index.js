c30eed33ee9680647a3e7a6d3d61b1a7a67ce7c1
Debi Ejeta <debimulugeta1999@gmail.com>
2024-05-08 14:23:45 -0400

    made changes

--- function-http/index.js
+++
@@ -1,6 +0,0 @@
-const {HttpFunction} = require('@google-cloud/functions-framework');
-
-// Register an HTTP function with the Functions Framework
-HttpFunction('helloHttp', (req, res) => {
-  res.send('Hello from Google Cloud Functions!');
-});
