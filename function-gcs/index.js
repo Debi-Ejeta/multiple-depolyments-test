448fb2a93d4e178deaa3aee69aeedc643ac039cb
Debi Ejeta <86627766+Debi-Ejeta@users.noreply.github.com>
2024-05-08 14:03:14 -0400

    Update index.js

--- function-gcs/index.js
+++ function-gcs/index.js
@@ -2,8 +2,6 @@ const {CloudEvents} = require('@google-cloud/functions-framework');
 
 // Register a CloudEvent function with the Functions Framework
 exports.helloGCS = (cloudEvent) => {
-  console.log(cloudEvent);
-  const file = cloudEvent.data;
-  console.log(file);
-  console.log(`File ${file.name} was uploaded to ${file.bucket}`);
+  console.log("Cloud EVent:", cloudEvent);
+  console.log(`File ${cloudEvent.name} was uploaded to ${cloudEvent.bucket}`);
 };
